46c491940844e5f5944061611bc733856b8e9298
LAPTOP-K6DC54CT\cd201 <dmswls8988@gmail.com>
2023-01-31 16:51:04 +0900

    add Day3 solution file

---
+++ Day3/day3-2.js
@@ -0,0 +1,9 @@
+function solution(array) {
+
+    const mid_num = Math.floor(array.length / 2);
+  
+  
+    const jjin_array = array.sort((a, b) => a - b);
+  
+    return jjin_array[mid_num];
+  }
