6401e114e782b4b2d6d1272e56702826ba8e5e56
LAPTOP-K6DC54CT\cd201 <dmswls8988@gmail.com>
2023-02-01 13:27:14 +0900

    add Day2 solution file

--- Day2/day2-3.js
+++ Day2/day2-3.js
@@ -1,22 +1,16 @@
 function solution(numer1, denom1, numer2, denom2) {
     var answer = [];
     
-    var mo = denom1 * denom2
-    var ja = (numer1 * denom2) + (numer2 * denom1)
+    var denom = denom1 * denom2
+    var numer = (numer1 * denom2) + (numer2 * denom1)
     
-    var max = Math.max(mo, ja);
-    var min = Math.min(mo, ja);
-    
-    while(min!=0) {
-        var a = max%min;
-        max = min;
-        min = a;
+    let a = 1
+ 
+    for(let i = 1 ; i <= numer ; i ++) {
+        if(numer%i === 0 && denom%i === 0) {
+            a = i
+        }
     }
+    return [numer/a, denom/a]
     
-    answer[0] = mo/max;
-    answer[1] = ja/max;
-    
-    return answer;
-    
-    //다시풀어야하는문제
 }
