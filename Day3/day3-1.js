335d4e10298f06c1754da2b6706b7ae9dfea95de
LAPTOP-K6DC54CT\cd201 <dmswls8988@gmail.com>
2023-02-01 14:03:25 +0900

    add Day3 solution file - 2022.02.01

---
+++ Day3/day3-1.js
@@ -0,0 +1,16 @@
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
+    
+    var denom = denom1 * denom2
+    var numer = (numer1 * denom2) + (numer2 * denom1)
+    
+    let a = 1
+ 
+    for(let i = 1 ; i <= numer ; i ++) {
+        if(numer%i === 0 && denom%i === 0) {
+            a = i
+        }
+    }
+    return [numer/a, denom/a]
+    
+}
